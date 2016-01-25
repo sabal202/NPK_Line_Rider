41d524042b2abaf5eb063eee0e6789d752096303
Sebekerga <sebekerka@gmail.com>
2016-01-25 21:54:44 +0600

    lkolplplplpl

--- helloOpenCV/src/main/java/com/sabal/opencv/Robot2WD.java
+++ helloOpenCV/src/main/java/com/sabal/opencv/Robot2WD.java
@@ -12,5 +12,4 @@ public interface Robot2WD {
     void MotorsPowerSet(byte PowerA,byte PowerB) throws IOException;
     void MotorsPowerOff() throws IOException;
     void MotorsPowerOn() throws IOException;
-////546454545454544545454545454545454545454554545454544545445454545454544554455445454455454455454454
 }

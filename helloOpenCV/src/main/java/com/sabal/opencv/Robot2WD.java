9169769cb3c9c0a1fada5ef53089e220369f9504
vasv03 <vasv03@gmail.com>
2016-02-03 15:22:56 +0500

    методы подключения/отключения могут вызвать ошибку

--- helloOpenCV/src/main/java/com/sabal/opencv/Robot2WD.java
+++ helloOpenCV/src/main/java/com/sabal/opencv/Robot2WD.java
@@ -7,8 +7,8 @@ import java.io.IOException;
  */
 public interface Robot2WD {
 
-    boolean Connect(String Name);
-    void Disconnect();
+    boolean Connect(String Name) throws IOException;
+    void Disconnect() throws IOException;
     void MotorsPowerSet(byte PowerA,byte PowerB) throws IOException;
     void MotorsPowerOff() throws IOException;
     void MotorsPowerOn() throws IOException;
